52a69d0ed2e3c37c0623a4bc560715ad5739b04b
chouchouji <70570907+chouchouji@users.noreply.github.com>
2025-01-02 07:15:15 +0000

    feat: support dynamic import

--- src/lib/rules/import-alias.js
+++ src/lib/rules/import-alias.js
@@ -7,7 +7,7 @@ export const meta = {
     recommended: true
   },
   fixable: 'code',
-  schema: [ {
+  schema: [{
     type: 'object',
     properties: {
       rootDir: { type: 'string' },
@@ -27,7 +27,7 @@ export const meta = {
         }
       }
     }
-  } ]
+  }]
 };
 
 const RELATIVE_MATCHER = /^(?:\.{1,2}\/)+/;
@@ -43,7 +43,7 @@ function getDepthCount(matchedPath) {
 export function create(context) {
   const {
     relativeDepth = -1,
-    aliases:_aliases = [],
+    aliases: _aliases = [],
     rootDir = CWD
   } = context.options[0] || {};
 
@@ -82,13 +82,49 @@ export function create(context) {
                     + item.alias
                     + importPath.slice(index + matchingString.length);
 
-                  return fixer.replaceTextRange([ node.source.range[0] + 1, node.source.range[1] - 1 ], result);
+                  return fixer.replaceTextRange([node.source.range[0] + 1, node.source.range[1] - 1], result);
                 }
               }
             }
           });
         }
       }
-    }
+    },
+    ImportExpression(node) {
+      /** @type {string} */
+      const importValue = node.source.value;
+      const matches = importValue.match(RELATIVE_MATCHER);
+
+      if (matches) {
+        const depth = getDepthCount(matches[0]);
+
+        if (depth > relativeDepth) {
+          context.report({
+            node,
+            message: relativeDepth === -1
+              ? 'Import path mush be a path alias'
+              : `import statement must be an alias or no more than ${relativeDepth} levels deep`,
+            fix(fixer) {
+              const parsedPath = path.parse(context.getFilename());
+              const importPath = path.relative(rootDir, path.resolve(parsedPath.dir, importValue));
+
+              for (const item of aliases) {
+                const match = importPath.match(item.matcher);
+
+                if (match) {
+                  const matchingString = match[match.length - 1];
+                  const index = match[0].indexOf(matchingString);
+                  const result = importPath.slice(0, index)
+                    + item.alias
+                    + importPath.slice(index + matchingString.length);
+
+                  return fixer.replaceTextRange([node.source.range[0] + 1, node.source.range[1] - 1], result);
+                }
+              }
+            }
+          });
+        }
+      }
+    },
   }
 }
