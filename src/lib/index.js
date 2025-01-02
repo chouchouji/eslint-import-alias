ac0aaf8fd74d49a70416f43e46c01a9d9d9bedd5
chouchouji <70570907+chouchouji@users.noreply.github.com>
2025-01-02 07:39:11 +0000

    fix: update export

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,11 @@
 import * as ImportAliasRule from './rules/import-alias';
 
-export const rules = {
-  'import-alias': ImportAliasRule
+export default {
+  configs: {
+    recommended: {
+      plugins: ['@binbinji/import-alias'],
+      rules: { '@binbinji/import-alias/import-alias': 'error' },
+    },
+  },
+  rules: { 'import-alias': ImportAliasRule },
 };
